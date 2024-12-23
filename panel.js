51455826bb21aa62083d24032ecf0175b008d962
BitPod.AI <BitPodAI@gmail.com>
2024-12-24 06:51:55 +0800

    Update tray btn

--- panel.js
+++ panel.js
@@ -5,15 +5,15 @@ chatbotIcon.id = 'panel-icon';
 chatbotIcon.style.position = 'fixed';
 chatbotIcon.style.bottom = '60px';
 chatbotIcon.style.right = '10px';
-chatbotIcon.style.width = '60px';
-chatbotIcon.style.height = '60px';
+chatbotIcon.style.width = '45px';
+chatbotIcon.style.height = '45px';
 //chatbotIcon.style.backgroundColor = '#007bff';
 chatbotIcon.style.borderRadius = '50%';
 chatbotIcon.style.cursor = 'pointer';
 chatbotIcon.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
 chatbotIcon.style.zIndex = '9000';
 const img = document.createElement('img');
-img.src = chrome.runtime.getURL('public/logo.gif');
+img.src = chrome.runtime.getURL('public/tray.gif');
 img.style.cssText = `
       width: 100%;
       height: 100%;
