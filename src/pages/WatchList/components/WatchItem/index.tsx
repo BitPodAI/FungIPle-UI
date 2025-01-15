9f8c845b3fdacb256c39376df63f71ea3d69e956
victor <40204871+robin-fc@users.noreply.github.com>
2025-01-16 01:40:41 +0800

    perf: import ToastContainer in APP.tsx & useShare

--- src/pages/WatchList/components/WatchItem/index.tsx
+++ src/pages/WatchList/components/WatchItem/index.tsx
@@ -6,32 +6,15 @@ import MemoSVG from '@/assets/icons/memo.svg';
 import TranslateSVG from '@/assets/icons/translate.svg';
 import CopySVG from '@/assets/icons/copy.svg';
 import RefreshSVG from '@/assets/icons/refresh.svg';
-import { watchApi } from '@/services/watch';
-import { useUserStore } from '@/stores/useUserStore';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import useShare from '@/hooks/useShare';
 
 export const WatchItem: React.FC<Message> = ({ text, user, title, updatedAt }) => {
   const isUser = user === 'user';
-  const userId = useUserStore.getState().getUserId();
-  const notify = () => toast("Shared on X");
-
-  const handleShareClick = (text: string) => {
-    console.log(text);
-    notify();
-    watchApi.reTweeted(text, userId?userId:'');
-  };
+  const { handleShareClick } = useShare();
 
   return (
     <div className={`flex justify-center mb-4 animate-fade-in`}>
-    <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        pauseOnHover={false}
-        theme="colored"
-      />
       <div
         className={`flex flex-col max-w-[80%] items-center px-6 py-2 ${
           isUser
@@ -40,21 +23,21 @@ export const WatchItem: React.FC<Message> = ({ text, user, title, updatedAt }) =
         }`}
       >
         {updatedAt && (
-          <p className="w-full text-[12px] inknut-antiqua text-gray-600" style={{ textAlign: 'left' }}>
+          <p className="w-full text-[12px] averia-serif-libre text-gray-600" style={{ textAlign: 'left' }}>
             {updatedAt}
           </p>
         )}
         {title && (
-          <p className="w-full text-[14px] inknut-antiqua" style={{ textAlign: 'left', fontWeight: 'bold' }}>
+          <p className="w-full text-[14px] averia-serif-libre" style={{ textAlign: 'left', fontWeight: 'bold' }}>
             {title}
           </p>
         )}
         {title ? (
-          <p className="text-[12px] inknut-antiqua text-gray-800" style={{ whiteSpace: 'pre-line' }}>
+          <p className="text-[12px] averia-serif-libre text-gray-800" style={{ whiteSpace: 'pre-line' }}>
             {text}
           </p>
         ) : (
-          <p className="text-[12px] inknut-antiqua">{text}</p>
+          <p className="text-[12px] averia-serif-libre">{text}</p>
         )}
         {!isUser && (
           <div className="w-full flex items-center justify-end gap-4">
