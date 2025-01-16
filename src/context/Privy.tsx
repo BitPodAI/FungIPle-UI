7a81de9fbf863cb218c2a364ee19591762726514
waite <waite1x@outlook.com>
2025-01-16 21:29:47 +0800

    feat:  Add wallect connet

--- src/context/Privy.tsx
+++ src/context/Privy.tsx
@@ -8,9 +8,19 @@ export default function AppPriviyProvider({ children }: { children: React.ReactN
           accentColor: '#000000',
           theme: '#FFFFFF',
           showWalletLoginFirst: false,
+          walletList: ['detected_wallets','wallet_connect', 'coinbase_wallet'],
           logo: 'https://auth.privy.io/logos/privy-logo.png',
+          walletChainType: 'ethereum-and-solana',
         },
-        loginMethods: ['google', 'twitter'],
+        loginMethods: ['google', 'twitter', 'wallet'],
+        externalWallets: {
+          walletConnect: {
+            enabled: true,
+          },
+          coinbaseWallet: {
+            connectionOptions: 'all',
+          },
+        },
         fundingMethodConfig: {
           moonpay: {
             useSandbox: true,
