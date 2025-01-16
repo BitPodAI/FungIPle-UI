08ef225bab157dbd28162b2f6ad0d65f2ae88db8
robin-fc <873602646@qq.com>
2025-01-16 19:11:56 +0800

    feat: import gif

--- src/config/image.ts
+++ src/config/image.ts
@@ -1,7 +1,13 @@
-import agent1_1 from '@/assets/images/agent/1-1.png';
-import agent1_2 from '@/assets/images/agent/1-2.png';
-import agent1_3 from '@/assets/images/agent/1-3.png';
-import agent1_4 from '@/assets/images/agent/1-4.png';
-import agent1_5 from '@/assets/images/agent/1-5.png';
+import agent1_1_0 from '@/assets/images/agent/1-1-0.png';
+import agent1_2_0 from '@/assets/images/agent/1-2-0.png';
+import agent1_3_0 from '@/assets/images/agent/1-3-0.png';
+import agent1_4_0 from '@/assets/images/agent/1-4-0.png';
+import agent1_5_0 from '@/assets/images/agent/1-5-0.png';
+import agent2_1_0 from '@/assets/images/agent/2-1-0.png';
+import agent2_2_0 from '@/assets/images/agent/2-2-0.png';
+import agent2_3_0 from '@/assets/images/agent/2-3-0.png';
+import agent2_4_0 from '@/assets/images/agent/2-4-0.png';
+import agent2_5_0 from '@/assets/images/agent/2-5-0.png';
 
-export const AGENT_IMAGES_1 = [agent1_1, agent1_2, agent1_3, agent1_4, agent1_5];
+export const AGENT_IMAGES_1 = [agent1_1_0, agent1_2_0, agent1_3_0, agent1_4_0, agent1_5_0];
+export const AGENT_IMAGES_2 = [agent2_1_0, agent2_2_0, agent2_3_0, agent2_4_0, agent2_5_0];
