ff48bffa58813f733603b86c1c58fcfea4a79b5f
victor <40204871+robin-fc@users.noreply.github.com>
2025-01-17 00:52:17 +0800

    fix: add route match

--- src/layout/PluginLayout/components/Sidebar/index.tsx
+++ src/layout/PluginLayout/components/Sidebar/index.tsx
@@ -1,11 +1,37 @@
 import IconWrap from '@/components/common/IconWrap';
 import { Link } from 'react-router-dom';
 import { mainNavs, extendNavs } from './config';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MAIN_NAVS, EXTEND_NAVS, OTHER_NAVS } from '@/constant/navs';
+import { useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
-  const [selectedNav, setSelectedNav] = useState<MAIN_NAVS | EXTEND_NAVS | OTHER_NAVS>(MAIN_NAVS.CHAT);
+  const location = useLocation();
+  const [selectedNav, setSelectedNav] = useState<string>();
+
+  useEffect(() => {
+    // Update selectedNav based on the current route
+    const path = location.pathname;
+    switch (path) {
+      case '/plugin/chat':
+        setSelectedNav(MAIN_NAVS.CHAT);
+        break;
+      case '/plugin/agent':
+        setSelectedNav(MAIN_NAVS.AGENT);
+        break;
+      case '/plugin/watch-list':
+        setSelectedNav(MAIN_NAVS.WATCH_LIST);
+        break;
+      case '/plugin/discover':
+        setSelectedNav(MAIN_NAVS.DISCOVER);
+        break;
+      case '/plugin/memo':
+        setSelectedNav(EXTEND_NAVS.MEMO);
+        break;
+      default:
+        setSelectedNav(OTHER_NAVS.HELP);
+    }
+  }, [location]);
 
   return (
     <div className="w-[60px] h-screen py-[16px] box-border flex flex-col justify-between items-center bg-[#F7F6F5]">
