e312d159ef6278a8ffc421d1bb4590074d65d103
ykai2 <yangykai@foxmail.com>
2025-02-08 16:26:11 +0800

    As long as the X account is authenticated, the sharing function can be used.
    Signed-off-by: ykai2 <yangykai@foxmail.com>

--- src/hooks/useShare.ts
+++ src/hooks/useShare.ts
@@ -3,25 +3,21 @@ import { useUserStore } from '@/stores/useUserStore';
 import { toast } from 'react-toastify';
 
 const useShare = () => {
-  const userId = useUserStore.getState().getUserId();
-  const enable = useUserStore.getState().userProfile?.agentCfg?.enabled;
 
   const handleShareClick = (text: string) => {
-    toast('Shared on X');
-    watchApi.reTweeted(text, userId ? userId : '');
+    const userId = useUserStore.getState().getUserId();
+    const twitterToken = useUserStore.getState().userProfile?.tweetProfile?.accessToken;
+    if (userId && twitterToken) {
+      toast('Shared on X');
+      watchApi.reTweeted(text, userId ? userId : '');
+    } else {
+      toast('Please authorize your X account on the Agent page first.');
+    }
   };
 
-  if (!enable) {
-    return {
-      handleShareClick: () => {
-        toast('The switch isn\'t enabled yet. Please enable the \"X Takeover by Agent\" switch to continue.');
-      },
-    };
-  } else {
-    return {
-      handleShareClick,
-    };
-  }
+  return {
+    handleShareClick,
+  };
 };
 
 export default useShare;
