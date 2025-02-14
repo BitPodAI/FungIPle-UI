7d583f8eae14d03cb7e8f106b92727a91afe272e
Auden <49225795+zhongchunfengyyds@users.noreply.github.com>
2025-02-14 16:23:42 +0800

    kol

--- src/pages/Popup/login.tsx
+++ src/pages/Popup/login.tsx
@@ -5,11 +5,9 @@ const LoginPopup = () => {
   const { login, user, getAccessToken, ready } = usePrivy();
 
   const handleAuth = async () => {
-    // 如果已登录，获取用户信息并发送
     const token = await getAccessToken();
-    window.opener.postMessage({ type: 'GOOGLE_AUTH_SUCCESS', data: { ...user, token } }, '*'); // 第二个参数是目标窗口的origin，' * ' 允许任何来源
-    window.close(); // 登录成功后关闭弹窗
-  };
+    window.opener.postMessage({ type: 'GOOGLE_AUTH_SUCCESS', data: { ...user, token } }, '*');
+    window.close();
 
   const handleLogin = async () => {
     await login();
